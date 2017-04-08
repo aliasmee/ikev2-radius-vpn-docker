4e10b735f955224b2c567001790f660150b5c000
aliasmee <xhanyifeng@gmail.com>
2017-04-08 13:28:20 +0800

    First commit

--- Dockerfile
+++ Dockerfile
@@ -16,10 +16,11 @@ RUN tar -xf strongswan-5.5.2.tar.gz
 # Configure,make and install
 RUN cd /strongswan-5.5.2 && ./configure  --enable-eap-identity --enable-eap-md5 --enable-eap-mschapv2 --enable-eap-tls --enable-eap-ttls --enable-eap-peap --enable-eap-tnc --enable-eap-dynamic --enable-eap-radius --enable-xauth-eap --enable-xauth-pam  --enable-dhcp  --enable-openssl  --enable-addrblock --enable-unity --enable-certexpire --enable-radattr --enable-swanctl --enable-openssl --disable-gmp && make && make install
 
-RUN rm -rf /strongswan-5.5.2
+RUN rm -rf /strongswan-5.5.2 && rm -f /strongswan-5.5.2.tar.gz
+RUN \cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime 
 
 # Open udp 500\4500 port
 EXPOSE 500:500/udp
 EXPOSE 4500:4500/udp
 #CMD ["/usr/bin/initvpn.sh","start"]
-CMD ["/bin/echo","hello"]
+CMD ["/usr/bin/echo","hello"]
---
+++ scripts/vpn.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Author by Yifeng Han
+#Time 2017-04-08 13:00
+
+# Create certificate
+
+mkdir /data/key_files && cd /data/key_files
+ipsec pki --gen --outform pem > ca.pem 
+ipsec pki --self --in ca.pem --dn "C=cn, O=ilove, CN=jdcloud vpn" --ca --lifetime 3650 --outform pem >ca.cert.pem 
+ipsec pki --gen --outform pem > server.pem 
+ipsec pki --pub --in server.pem | ipsec pki --issue --lifetime 1200 --cacert ca.cert.pem --cakey ca.pem --dn "C=cn, O=ilove, CN=202.77.130.178" --san="202.77.130.178" --flag serverAuth --flag ikeIntermediate --outform pem > server.cert.pem 
+ipsec pki --gen --outform pem > client.pem 
+ipsec pki --pub --in client.pem | ipsec pki --issue --cacert ca.cert.pem --cakey ca.pem --dn "C=cn, O=ilove, CN=jdcloud vpn client" --outform pem > client.cert.pe
+
+# Copy certificate to ipsec dir
+\cp ca.cert.pem /usr/local/etc/ipsec.d/cacerts/ 
+\cp server.cert.pem /usr/local/etc/ipsec.d/certs/ 
+\cp server.pem /usr/local/etc/ipsec.d/private/ 
+\cp client.cert.pem /usr/local/etc/ipsec.d/certs/ 
+\cp client.pem  /usr/local/etc/ipsec.d/private/
+
+# Enable system forward
+sysctl -w net.ipv4.ip_forward=1
+
+# Disable firewalld and Enable iptables
+systemctl stop firewalld && systemctl disable firewalld && systemctl start iptables && systemctl enable iptables
+
+# Start ipsec
+ipsec start
